80c611920d287654c69d5e732fe6ff256362c805
Harshith Kulkarni <harshithkulkarni@gmail.com>
2019-01-09 17:57:30 +0530

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -1,18 +1,14 @@
 import sqlite3
 
-
-print("SQL Database creating...")
-conn = sqlite3.connect("Database.db")
-print("SQL Database Created Successfully!!")
-c = conn.cursor()
-c.execute('''CREATE TABLE `1` (`Name`	REAL,`Email`	REAL,`Gender`	REAL,`Phone Number`	REAL)''')
-
-"""c_file = open("c_file.txt","w")
-c = str(c)
-c_file.write(c)
-c_file.close()
-
-conn_file = open("conn_file.txt","w")
-conn = str(conn)
-conn_file.write(conn)
-conn_file.close()"""
+class database:
+	def __init__(self):
+		pass
+	def create_db(self):
+		print("SQL Database creating...")
+		self.conn = sqlite3.connect("Database.db")
+		print("SQL Database Created Successfully!!")
+		self.c = self.conn.cursor()
+	def create_table(self):
+		self.c.execute('''CREATE TABLE `1` (`Name`	REAL,`Email`	REAL,`Gender`	REAL,`Phone Number`	REAL)''')
+		return self.c , self.conn
+	
